a8478ac9d9f37f2fce0870b35736549ba23cd42b
Jacob <whyisjacob@gmail.com>
2019-06-21 11:29:42 -0600

    initial commit

---
+++ hello.py
@@ -0,0 +1,8 @@
+num1 = float( input("Enter the first number: "))
+num2 = float( input("Enter the second number: "))
+num3 = float( input("Enter the third number: "))
+
+
+
+print("The max value is: ", max(num1,num2,num3))
+input("Press any key to exit")
